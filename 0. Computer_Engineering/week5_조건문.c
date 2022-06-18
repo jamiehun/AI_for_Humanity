5bebb27cd09aad6eb97b19500954e5fd23c86a6c
jamiehun <jamiesunghun@gmail.com>
2022-06-18 19:02:11 +0900

    week5_complete

--- 0. Computer_Engineering/week5_조건문.c
+++ 0. Computer_Engineering/week5_조건문.c
@@ -1,5 +1,4 @@
 // if-else 예제 : 2개의 값을 입력받아 짝수와 홀수를 판별
-/*
 #include <stdio.h>
 int main(void)
 {
@@ -16,11 +15,11 @@ int main(void)
     return 0;
 
 }
-*/
+
 
 // -------------------------------------------------
 // if-else 예제 : 2개의 값을 입력받아 큰 값 (max)를 출력
-/*
+
 #include <stdio.h>
 int main(void)
 {
@@ -41,7 +40,7 @@ int main(void)
 
 // -------------------------------------------------
 // if-else 예제 : 3개의 값을 입력받아 가장 작은 값(min)을 출력
-/*
+
 #include <stdio.h>
 int main(void)
 {
@@ -64,9 +63,10 @@ int main(void)
     printf("The minimum value is %d\n", min);
     return 0;
 }
-*/
+
 // -------------------------------------------------
 // if-else 예제 : 점수를 입력받아 성적을 출력
+
 #include <stdio.h>
 int main(void)
 {
@@ -92,4 +92,50 @@ int main(void)
     }
     printf("수고 많으셨습니다!\n");
     return 0;
+}
+
+// -------------------------------------------------
+// switch 예제 : 윤년 계산
+#include <stdio.h>
+int main(void){
+int year = 0, month = 0, maxDay = 0;
+printf("* 년과 월을 입력하세요 : ");
+scanf("%d %d", &year, &month);
+switch(month)
+{
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        maxDay = 31;
+        printf("%d년 %d월의 말일은 %d일 입니다. \n", year, month, maxDay);
+        break;
+    
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        maxDay = 30;
+        printf("%d년 %d월의 말일은 %d일 입니다. \n", year, month, maxDay);
+    
+    case 2:
+        if((year%4 ==0) && (year%100 != 0) || (year%400 == 0))
+        {
+            maxDay = 29;
+            printf("%d년 %d월의 말일은 %d일(윤년)입니다.\n", year, month, maxDay);
+            break;
+        }
+        else
+        {
+            maxDay = 28;
+            printf("%d년 %d월의 말일은 %d일 입니다.\n", year, month, maxDay);
+            break;
+        }
+    default:
+    printf("입력이 잘못 되었습니다!\n");
+}
+return 0;
 }
