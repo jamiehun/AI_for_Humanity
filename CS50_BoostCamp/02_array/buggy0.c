0307e051b6fd365a6a1ec80b723b68e4ae6ba3f4
jamiehun <jamiesunghun@gmail.com>
2022-07-14 16:21:23 +0900

    CS50_220714

---
+++ CS50_BoostCamp/02_array/buggy0.c
@@ -0,0 +1,6 @@
+""" cs50의 help50을 통해 디버깅 """
+""" vs code에서는 작동 안함 """
+int main(void)
+{
+    printf("Hello, World!\n");
+}
---
+++ CS50_BoostCamp/02_array/buggy1.c
@@ -0,0 +1,7 @@
+""" cs50의 help50을 통해 디버깅 """
+""" vs code에서는 작동 안함 """
+int main(void)
+{
+    string answer = get_string("What is your name?: \n");
+    printf("My name is %s\n", answer);
+}
